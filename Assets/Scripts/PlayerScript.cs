c4868d3b0a072f47db027bdb15be9a1b13c3c469
HIROpen <hiropen1996@gmail.com>
2016-06-20 15:41:43 +0900

    first commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour {
+        public static int score = 0;
+        public Text scoreText;
+
+        void Update () {
+                if(Input.GetMouseButtonDown(0)) {
+                        GetComponent<Rigidbody>().velocity = new Vector3(0, 6, 0);
+                        GetComponent<AudioSource>().Play();
+                }
+                scoreText.text = "Score: "+score.ToString();
+        }
+        void OnTriggerExit(Collider other){
+                score++;
+        }
+        void OnCollisionEnter(Collision other){
+                score = 0;
+                SceneManager.LoadScene ("result");
+        }
+}
